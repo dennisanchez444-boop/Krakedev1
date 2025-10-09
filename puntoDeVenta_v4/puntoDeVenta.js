56ec16289f64657eb0056e07e691ea29fff8e41f
dennisanchez444-boop <dennisanchez444@gmail.com>
2025-10-09 15:54:07 -0500

    Funcion limpiar completada

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -94,6 +94,16 @@ calcularValorTotal = function () {
 
 }
 limpiar = function () {
+    mostrarTextoEnCaja("txtProducto","");
+    mostrarTextoEnCaja("txtCantidad","0");
+    mostrarTextoEnCaja("txtPrecio","0.0");
+    mostrarTextoEnCaja("txtPorcentajeDescuento","0");
+    mostrarTexto("lblSubtotal","0.0")
+    mostrarTexto("lblDescuento","0.0")
+    mostrarTexto("lblValorIVA","0.0")
+    mostrarTexto("lblTotal","0.0")
+    mostrarTexto("lblResumen","")
+
     /*
         Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
         Dejar todos los textos de los montos con el valor 0.0
