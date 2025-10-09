2e47d3d68b6400b9c053f64077b0435ff18ce48b
dennisanchez444-boop <dennisanchez444@gmail.com>
2025-10-09 15:24:27 -0500

    Completado el IVA esperado

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -46,9 +46,11 @@ calcularValorTotal = function () {
         Si el caso de prueba es exitoso, hacer un commit
      */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
+    valorIVA=calcularIva(valorSubtotal-valorDescuento);
     // El IVA debe calcularse sobre el valor del subtotal menos el descuento
     //9. Mostrar el resultado en el componente lblValorIVA    
-        /*
+    mostrarTexto("lblValorIVA",valorIVA);
+    /*
             Caso de prueba: 
                 - cantidad: 10 
                 - precioProducto: 5.4  
