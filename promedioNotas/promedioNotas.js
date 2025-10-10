3e534a9501528806f456013136e744675e1f5055
dennisanchez444-boop <dennisanchez444@gmail.com>
2025-10-10 10:22:42 -0500

    Todo funciona

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -2,13 +2,14 @@ calcularPromedioNotas=function(){
     let nota1=recuperarFloat("txtNota1");
     let nota2=recuperarFloat("txtNota2");
     let nota3=recuperarFloat("txtNota3");
-    
-    let reprobado="REPROBADO";
-    let buenTrabajo="BUEN TRABAJO";
-    let exelente="EXELENTE";
-    let datos="DATOS INCORRECTOS";
 
     promedio=calcularPromedio(nota1,nota2,nota3);
 
     let resultado=promedio.toFixed(2);
+
+    if(resultado>7){
+        mostrarImagen("imgImagen","tenor.gif");
+    }else{
+        mostrarImagen("imgImagen","giphy.gif");
+    }
 }
