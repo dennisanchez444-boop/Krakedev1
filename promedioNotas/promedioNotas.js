875b6f548d482cbf4ae8402e1962d3d00ee07e64
dennisanchez444-boop <dennisanchez444@gmail.com>
2025-10-17 10:17:31 -0500

    Promedio notas listo

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -3,13 +3,26 @@ calcularPromedioNotas=function(){
     let nota2=recuperarFloat("txtNota2");
     let nota3=recuperarFloat("txtNota3");
 
+    let reprobado="REPROBADO";
+    let buenTrabajo="BUEN TRABAJO";
+    let exelente="EXELENTE";
+    let datos="DATOS INCORRECTOS";
+
     promedio=calcularPromedio(nota1,nota2,nota3);
 
     let resultado=promedio.toFixed(2);
 
-    if(resultado>7){
-        mostrarImagen("imgImagen","tenor.gif");
+    if(resultado <  5 && resultado > 0){
+        mostrarTexto("txtDato",reprobado);
+        mostrarImagen("imgImagen","reprobado.gif");
+    }else if(resultado >=  5 && resultado <= 8){
+        mostrarTexto("txtDato",buenTrabajo);
+        mostrarImagen("imgImagen","buenTrabajo.gif");
+    }else if(resultado > 8 && resultado <= 10){
+        mostrarTexto("txtDato",exelente);
+        mostrarImagen("imgImagen","exelente.gif");
     }else{
-        mostrarImagen("imgImagen","giphy.gif");
+        mostrarTexto("txtDato",datos);
+        mostrarImagen("imgImagen","error.gif");
     }
 }
