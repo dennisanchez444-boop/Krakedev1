54d3543b1c662155eab23364bb7a36ebb2d7cb1b
dennisanchez444-boop <dennisanchez444@gmail.com>
2025-10-22 09:18:50 -0500

    Obtener provincia Listo

--- placas/placas.js
+++ placas/placas.js
@@ -1,5 +1,4 @@
 validarPlaca = function () {
-    
     let valores = recuperarTexto("txtValor");
     variableErroresEStructura = validarEstructura(valores);
     if (variableErroresEStructura == "") {
@@ -7,5 +6,11 @@ validarPlaca = function () {
     } else {
         mostrarTexto("lblDatos", "ESTRUCTURA INCORRECTA");
         mostrarTexto("lblError", variableErroresEStructura);
+    }
+    let provincia = obtenerProvincia(valores)
+    if (provincia == null) {
+        mostrarTexto("lblProvincias", "Provincia incorrecta");
+    } else {
+        mostrarTexto("lblProvincias", "La placa pertenece a la provincia de: " + provincia);
     }
 }
--- placas/serviciosPlacas.js
+++ placas/serviciosPlacas.js
@@ -32,3 +32,87 @@ validarEstructura = function (placa) {
     }
     return errores;
 }
+
+obtenerProvincia = function (placa) {
+    let provincia;
+    if (placa.charAt(0) == "A") {
+        provincia = "Azuay";
+    }
+    else if (placa.charAt(0) == "B") {
+        provincia = "Bolivar";
+    }
+    else if (placa.charAt(0) == "U") {
+        provincia = "Canar";
+    }
+    else if (placa.charAt(0) == "C") {
+        provincia = "Carchi";
+    }
+    else if (placa.charAt(0) == "X") {
+        provincia = "Cotopaxi";
+    }
+    else if (placa.charAt(0) == "H") {
+        provincia = "Chimborazo";
+    }
+    else if (placa.charAt(0) == "O") {
+        provincia = "El Oro";
+    }
+    else if (placa.charAt(0) == "E") {
+        provincia = "Esmeraldas";
+    }
+    else if (placa.charAt(0) == "W") {
+        provincia = "Galapagos";
+    }
+    else if (placa.charAt(0) == "G") {
+        provincia = "Guayas";
+    }
+    else if (placa.charAt(0) == "I") {
+        provincia = "Imbabura"
+    }
+    else if (placa.charAt(0) == "L") {
+        provincia = "Loja";
+    }
+    else if (placa.charAt(0) == "R") {
+        provincia = "Los Rios";
+    }
+    else if (placa.charAt(0) == "M") {
+        provincia = "Manabi";
+    }
+    else if (placa.charAt(0) == "V") {
+        provincia = "Morona Santiago";
+    }
+    else if (placa.charAt(0) == "N") {
+        provincia = "Napo";
+    }
+    else if (placa.charAt(0) == "S") {
+        provincia = "Pastaza";
+    }
+    else if (placa.charAt(0) == "P") {
+        provincia = "Pichincha";
+    }
+    else if (placa.charAt(0) == "K") {
+        provincia = "Sucumbios";
+    }
+    else if (placa.charAt(0) == "Q") {
+        provincia = "Orellana";
+    }
+    else if (placa.charAt(0) == "T") {
+        provincia = "Tungurahua";
+    }
+    else if (placa.charAt(0) == "Z") {
+        provincia = "Zamora Chinchipe";
+    }
+    else if (placa.charAt(0) == "Y") {
+        provincia = "Santa Elena";
+    }
+    else if (placa.charAt(0) == "J") {
+        provincia = "Santo Domingo de los Tsachilas";
+    }
+    else {
+        provincia = null;
+    }
+    return provincia;
+
+}
+
+
+
